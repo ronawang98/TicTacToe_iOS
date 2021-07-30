0362b617e0a85e5b98149dd03ffada834a9dbe41
Rona Wang <ronawang98@gmail.com>
2021-07-30 15:49:32 +1000

    Create Xcode

---
+++ TicTacToe/TicTacToe/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Rona Wang on 30/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TicTacToe/TicTacToe/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  TicTacToe
+//
+//  Created by Rona Wang on 30/7/21.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
